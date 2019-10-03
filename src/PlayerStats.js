3f578ebe05a628cf11915f12f6b31badb8ce0693
cwperry <christopher.perry@employbridge.com>
2019-10-03 12:21:59 -0500

    Fixed lint of PlayerStats.js

--- src/PlayerStats.js
+++ src/PlayerStats.js
@@ -1,101 +1,95 @@
+/* eslint-disable react/forbid-prop-types */
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import Container from 'react-bootstrap/Container';
 
 import StatButton from './StatButton';
 
-import './PlayerStats.css'; 
+import './PlayerStats.css';
 
 class PlayerStats extends Component {
-
-    constructor(props) {
-        super(props);
-        this.handleStatChange = this.handleStatChange.bind(this);
-        this.state = {
-            player: this.props.player
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.handleStatChange = this.handleStatChange.bind(this);
+    const { player } = this.props;
+    this.state = {
+      player,
+    };
+  }
 
     handleShotOnGoalClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleShotsChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleShotsChanged(value));
+      this.handleStatChange();
     }
 
     handleMissedShotClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleMissedShotsChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleMissedShotsChanged(value));
+      this.handleStatChange();
     }
 
     handleGoalsForClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleGoalsForChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleGoalsForChanged(value));
+      this.handleStatChange();
     }
 
     handleGoalsAgainstClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleGoalsAgainstChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleGoalsAgainstChanged(value));
+      this.handleStatChange();
     }
 
     handleHustlesClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleHustlesChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleHustlesChanged(value));
+      this.handleStatChange();
     }
 
     handleHesitationsClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleHesitationsChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleHesitationsChanged(value));
+      this.handleStatChange();
     }
 
     handleTakeawaysClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleTakeawaysChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleTakeawaysChanged(value));
+      this.handleStatChange();
     }
 
     handleGiveawaysClick = (value) => {
-        const tempPlayer = this.props.player;
-        tempPlayer.handleGiveawaysChanged(value);
-        this.setState({player: tempPlayer});
-        this.handleStatChange();
+      this.setState((prevState) => prevState.player.handleGiveawaysChanged(value));
+      this.handleStatChange();
     }
 
     handleStatChange = () => {
-        this.props.onStatChange(this.state.player);
+      const { onStatChange } = this.props;
+      const { player } = this.state;
+      onStatChange(player);
     }
 
     render() {
-        return(
-            <div class="player-stats">
-                <Container fluid="true">
-                    <div class="player-header">
-                        <div class="name">{this.props.player.name}</div>
-                        <div class="number">{this.props.player.number}</div>
-                    </div>
-                    <StatButton buttonText="Shots" stat={this.props.player.stats.shotsOnGoal} onStatClick={this.handleShotOnGoalClick}/> 
-                    <StatButton variant="secondary" buttonText="Missed Shots" stat={this.props.player.stats.missedShots} onStatClick={this.handleMissedShotClick}/>  
-                    <StatButton buttonText="Goals For" stat={this.props.player.stats.goalsFor} onStatClick={this.handleGoalsForClick}/> 
-                    <StatButton variant="secondary" buttonText="Goals Against" stat={this.props.player.stats.goalsAgainst} onStatClick={this.handleGoalsAgainstClick}/>
-                    <StatButton buttonText="Hustles" stat={this.props.player.stats.hustles} onStatClick={this.handleHustlesClick}/>
-                    <StatButton variant="secondary" buttonText="Hesitations" stat={this.props.player.stats.hesitations} onStatClick={this.handleHesitationsClick}/>
-                    <StatButton buttonText="Takeaways" stat={this.props.player.stats.takeaways} onStatClick={this.handleTakeawaysClick}/>
-                    <StatButton variant="secondary" buttonText="Giveaways" stat={this.props.player.stats.giveaways} onStatClick={this.handleGiveawaysClick}/>
-                </Container>
+      const { player } = this.state;
+      return (
+        <div className="player-stats">
+          <Container fluid="true">
+            <div className="player-header">
+              <div className="name">{player.name}</div>
+              <div className="number">{player.number}</div>
             </div>
-            
-        );
+            <StatButton buttonText="Shots" stat={player.stats.shotsOnGoal} onStatClick={this.handleShotOnGoalClick} />
+            <StatButton buttonText="Missed Shots" stat={player.stats.missedShots} onStatClick={this.handleMissedShotClick} />
+            <StatButton buttonText="Goals For" stat={player.stats.goalsFor} onStatClick={this.handleGoalsForClick} />
+            <StatButton buttonText="Goals Against" stat={player.stats.goalsAgainst} onStatClick={this.handleGoalsAgainstClick} />
+            <StatButton buttonText="Hustles" stat={player.stats.hustles} onStatClick={this.handleHustlesClick} />
+            <StatButton buttonText="Hesitations" stat={player.stats.hesitations} onStatClick={this.handleHesitationsClick} />
+            <StatButton buttonText="Takeaways" stat={player.stats.takeaways} onStatClick={this.handleTakeawaysClick} />
+            <StatButton buttonText="Giveaways" stat={player.stats.giveaways} onStatClick={this.handleGiveawaysClick} />
+          </Container>
+        </div>
+
+      );
     }
 }
+
+PlayerStats.propTypes = {
+  player: PropTypes.object.isRequired,
+  onStatChange: PropTypes.func.isRequired,
+};
 
 export default PlayerStats;
